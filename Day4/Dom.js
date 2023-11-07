47ba7aa81bb47c736c2b022ae7685bfcee41d31a
MugundaanK <2k19cse059@kiot.ac.in>
2023-11-07 10:29:02 +0530

    Successfully Completed

--- Day4/Dom.js
+++ Day4/Dom.js
@@ -1,4 +1,33 @@
+// const p = document.getElementById("para");
+// // const t = document.getElementById("p");
+// // console.log(t);
+// const t1 = document.getElementById("a");
+// p.innerHTML="<b>Mugundaan</b>";
+// p.style.color = "green";
+// // t.style.color = "red";
+// t1.style.color="blue";
 
-const t = document.getElementById("para");
-console.log(t);
-t.style.color = "red";
+// let u = ["Mugunth","Karthik","Gowsick"];
+// const ul = document.getElementById('post');
+// for(user of u){
+//     ul.innerHTML += `<li><b> ${user} </b></li>`;
+// }
+
+// //EventListener:
+// function Clicked(){
+//     console.log("HI ! Mugundaan...");
+// }
+
+// //EventListener:
+// const p2=document.getElementById('p2');
+
+// function listener(){
+//     console.log("Bye! Mugundaan....");
+// }
+// p2.addEventListener('click',listener)
+
+// const t = document.getElementById('p');
+// function clicked(){
+//     t.style.color="red";
+// }
+// t.addEventListener('click',clicked);
---
+++ Task1/task.js
@@ -0,0 +1,37 @@
+//1
+let str = "I am enjoying my training";
+console.log(str.indexOf('my'));
+
+//2
+console.log(str.indexOf('m'));
+
+//3
+let s = str.slice(3);
+console.log(s);
+
+//4
+let s1 = str.slice(0,4);
+console.log(s1);
+
+//5
+console.log(str.replace('training',"journey"));
+
+//6
+for(let k in str){
+    console.log(str.charAt(k));
+}
+
+//7
+let s2 = "a,b,c";
+let a1 = s2.split(",");
+console.log(a1);
+
+//8
+let s3 = "   abc    ";
+console.log(s3);
+console.log(s3.trim());
+
+//9
+let a = " Hi!";
+let b = " Everyone...";
+console.log(a.concat(b));
